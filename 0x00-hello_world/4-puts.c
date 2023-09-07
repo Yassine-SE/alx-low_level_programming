702bc0fe76544938c24b1c9e006dd44fc1a14b27
yassine <ytarkaoui@gmail.com>
2023-09-07 10:35:58 -0700

    Task 4.2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include "stdio.h"
 
-/* Print text with puts function */
-int main (void)
+/** 
+ * Print text with puts function
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
