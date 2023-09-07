beeacf827de9bf0ad7810c1533c2bcea94b2e23c
yassine <ytarkaoui@gmail.com>
2023-09-07 11:50:54 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - something
+ *
+ * Return: something
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("size of a long int: %ld byte(s)", sizeof(long int));
+	printf("size of long long int: %lld byte(s)", sizeof(long long int);
+	printf("size of a float: %f byte(s)", sizeof(float));
+	return (0);
+}
