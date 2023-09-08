1f126339b9d4834d9995831d011e4bdaa9afa2f8
yassine <ytarkaoui@gmail.com>
2023-09-08 04:49:24 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "time.h"
+
+/**
+ * main - Check n if negative or positive
+ *
+ * Description: Using if statement to check n variable
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project is for learning variables, if else, and while
