87120e8ceefa0fcd7c956f32ebe83242d47f2d6e
yassine <ytarkaoui@gmail.com>
2023-09-08 10:51:08 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - Print numberz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
