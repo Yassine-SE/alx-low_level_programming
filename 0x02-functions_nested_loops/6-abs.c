e5536123681028428795cf325af1b05b71be3d8f
yassine <ytarkaoui@gmail.com>
2023-09-12 10:47:16 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+ * _abs - absolut value
+ * @v: return absolut
+ *
+ * Return: 0success
+ */
+int _abs(int v)
+{
+	if (v < 0)
+	{
+		v = (-1) * v;
+		return (v);
+	}
+	else
+	{
+		return (v);
+	}
+}
