e907552b863f6ffbb413a61dffff28e7ce21a24d
yassine <ytarkaoui@gmail.com>
2023-09-12 06:21:14 -0700

    Task 0

---
+++ 0x02-functions_nested_moops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "_putchar.h"
+
+/**
+ * main - printing putchar
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+	_putchar('Y');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_moops/_putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void _putchar(char character)
+{
+	putchar(character);
+	return;
+}
