57976433b818db89eaebab0b78f10fe33f2fa876
Yassine-SE <ytarkaoui@gmail.com>
2023-09-14 20:03:53 +0100

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+/**
+ * _isupper - is upper
+ * @c: param
+ * 
+ * Return: 0 success
+*/
+int _isupper(int c)
+{
+    if (c >= 65 && c <= 90)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
