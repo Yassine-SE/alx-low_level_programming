86767ca0fde6b570cdcedbfa48fe2ab82bb2b875
Yassine-SE <ytarkaoui@gmail.com>
2023-09-18 22:51:50 +0100

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: param
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i, j, count;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	for (j = count; j >= 0 ; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
