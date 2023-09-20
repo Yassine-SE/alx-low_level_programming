7865d2b45d2c341966b3703661ef84581b0f74df
Yassine-SE <ytarkaoui@gmail.com>
2023-09-20 20:10:36 +0100

    Task 1.2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,8 +21,10 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (j = 0; src[j] != '\0'; j++)
 		lenSRC++;
+
 	if (n < lenSRC)
 		lenSRC = n;
+
 	for ( m = 0; m < lenSRC; m++)
 	{
 		destination[len] = src[m];
