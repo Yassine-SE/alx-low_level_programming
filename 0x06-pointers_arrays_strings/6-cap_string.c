427281790a18fbd913efc1c543c5f448c60a70ed
Yassine-SE <ytarkaoui@gmail.com>
2023-09-21 18:33:51 +0100

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - function
+ * @s: param
+ *
+ * Return: 0 success
+*/
+char *cap_string(char *s)
+{	
+	int i, cap = 0;
+	char sep[] = {' ', ',', '.', '-', '"', '?', '!', '(', ')', '{', '}', '\n'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		int j;
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i] == '\t')
+				s[i] = ' ';
+			if (s[i] == sep[j])
+			{
+				if ('a' <= s[i + 1] && s[i + 1] <= 'z')
+					cap = 1;
+			}
+		}
+
+		if (cap && ('a' <= s[i] && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+			cap = 0;
+		}
+	}
+	return (s);
+}
