859d3f668c96933a5f7adaad09b90803ba9703de
Yassine-SE <ytarkaoui@gmail.com>
2023-09-28 16:46:18 +0100

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function
+ * @s: param
+ *
+ * Return: 0 success
+*/
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	
+	if (!*s)
+		return (0);
+
+	i++;
+	i += _strlen_recursion(s + 1);
+	return (i);
+}
