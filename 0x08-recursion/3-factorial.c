d4a06f8e65eeb234b05df12f7c57706f0485bbd8
Yassine-SE <ytarkaoui@gmail.com>
2023-09-28 17:37:56 +0100

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - function
+ * @n: function
+ *
+ * Return: 0 success
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n <= 0)
+		return (1);
+	
+	return (n * factorial(n - 1));
+}
