65db6e7e8dac705077b330a6a0cec9c4e1e5d2db
Yassine-SE <ytarkaoui@gmail.com>
2023-10-02 19:25:37 +0100

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - function
+ * @argc: param
+ * @argv: param
+ *
+ * Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc >= 0 && argv != NULL)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+		printf("\n");
+	}
+	return (0);
+}
