cb5367c6a4e437af7d7e4f3e800b875b97d9114a
Yassine-SE <ytarkaoui@gmail.com>
2023-10-04 19:15:18 +0100

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * alloc_grid - function
+ * @with: param
+ * @height: param
+ *
+ * Return: 0 success
+*/
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **dim;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	
+	dim = malloc(sizeof(int) * height);
+	if (!dim)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		dim[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+		{
+			dim[i][j] = 0;
+		}
+	}
+	return (dim);
+}
