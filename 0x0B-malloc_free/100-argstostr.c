9acf89b7c6f493b9f38ee37e5c0ea6272a344050
Yassine-SE <ytarkaoui@gmail.com>
2023-10-04 21:09:52 +0100

    Task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * argstostr - function
+ * @ac: param
+ * @av: param
+ *
+ * Return: 0 success
+*/
+char *argstostr(int ac, char **av)
+{
+	char *arr;
+	int i, j, b, c;
+	int count = 0;
+
+	if (ac == 0 || !av)
+		return (NULL);
+
+	j = 0;
+	for (i = 1; i < ac; i++)
+	{
+		while (av[i][j] != '\0')
+			j++;
+	}
+	j = j + ac;
+
+	arr = malloc(sizeof(char) * j + 1);
+	if (!arr)
+		return (NULL);
+
+	for (b = 1; b < ac; b++)
+	{
+		for (c = 0; av[b][c] != '\0'; c++)
+		{
+			arr[count] = av[b][c];
+			count++;
+		}
+		
+		if (arr[count] == '\0')
+			arr[count++] = '\n';
+	}
+	arr[count] = '\0';
+	return (arr);
+}
