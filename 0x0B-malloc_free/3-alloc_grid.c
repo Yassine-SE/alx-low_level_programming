8a68744d8cffcfc073c24a74a54abcc059e78133
Yassine-SE <ytarkaoui@gmail.com>
2023-10-04 19:24:29 +0100

    Task 3.3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	
+
 	dim = malloc(sizeof(int) * height);
 	if (!dim)
 		return (NULL);
@@ -24,10 +24,12 @@ int **alloc_grid(int width, int height)
 	{
 		dim[i] = malloc(sizeof(int) * width);
 		if (!dim[i])
+		{
 			for (; i >= 0; i--)
 				free(dim[i]);
 			free(dim);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			dim[i][j] = 0;
