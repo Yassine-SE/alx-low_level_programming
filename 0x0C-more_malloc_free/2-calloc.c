d5ada77a58ebddd1135576a1a23bc431a5f3431e
Yassine-SE <ytarkaoui@gmail.com>
2023-10-07 16:22:52 +0100

    Task 2.5

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include "stdlib.h"
 
+
+/**
+ * _set - function
+ * @s: param
+ * @c: param
+ * @n: param
+ *
+ * Return: 0 success
+*/
+char *_set(char *s, char c, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = c;
+
+	return (s);
+}
+
 /**
  * _calloc - function
  * @nmemb: param
@@ -20,10 +39,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!arr)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-	{
-		arr[i] = '0';
-	}
-
+	_set(arr, 0, nmemb);
 	return (arr);
 }
