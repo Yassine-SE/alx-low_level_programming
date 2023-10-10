977a1fe1aa0546858f53df62743a188bc3666af0
Yassine-SE <ytarkaoui@gmail.com>
2023-10-10 17:30:34 +0100

    Task 2.0

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * main - function
+ * @argc: param
+ * @argv: param
+ *
+ * Return 0 success
+*/
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);	
+}
