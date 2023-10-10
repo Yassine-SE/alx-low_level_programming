9f526bbf48e0528f9de4adbfb6ed1bbd0149dd58
Yassine-SE <ytarkaoui@gmail.com>
2023-10-10 19:07:19 +0100

    Task 1.0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - function
+ * @d: param
+ * @name: param
+ * @age: param
+ * @owner: param
+ *
+ * Return: 0 success
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return;
+}
