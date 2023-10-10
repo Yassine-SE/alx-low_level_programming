386413d849f5db209312ffc803e8cebf3720c34a
Yassine-SE <ytarkaoui@gmail.com>
2023-10-10 20:52:39 +0100

    Task 4.0

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+
+
+/**
+ * _strlen - function
+ * @str: param
+ *
+ * Return: 0 success
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * _strcpy - function
+ * @dest: param
+ * @src: param
+ *
+ * Return: 0 success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+		dest[i] = src[i];
+		i++;
+
+	return (dest);
+}
 
 /**
  * new_dog - function
@@ -9,11 +45,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *nv;
-	
-	nv->name = name;
-	nv->age = age;
-	nv->owner = owner;
+	dog_t *new;
+	int name_len = _strlen(name);
+	int owner_len = _strlen(owner);
 
-	return (nv);
+	new = malloc(sizeof(dog_t));
+	if (!new)
+		return (NULL);
+
+	new->name = malloc(sizeof(new->name) * (name_len + 1));
+	if (!new->name)
+		free(new);
+		return (NULL);
+
+	new->owner = malloc(sizeof(new->owner) * (owner_len + 1));
+	if (!new->owner)
+		free(new);
+		free(new->name);
+		return (NULL);
+
+	_strcpy(new->name, name);
+	_strcpy(new->owner, owner);
+	new->age = age;
+
+	return (new);
 }
