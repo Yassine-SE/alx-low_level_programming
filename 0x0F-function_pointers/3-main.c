9d145d08e49118f7e85e0ac2b00c158839f82004
Yassine-SE <ytarkaoui@gmail.com>
2023-10-11 23:22:40 +0100

    Task 3.0

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: param
+ * @argv: param
+ *
+ * Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int result = 0;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		result = (*get_op_func(argv[2]))(a, b);
+	}
+
+	printf("%d", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+
+/**
+ * op_add - function
+ * @a: param
+ * @b: param
+ *
+ * Return: 0 success
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function
+ * @a: param
+ * @b: param
+ *
+ * Return: 0 success
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function
+ * @a: param
+ * @b: param
+ *
+ * Return: 0 success
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function
+ * @a: param
+ * @b: param
+ *
+ * Return: 0 success
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function
+ * @a: param
+ * @b: param
+ *
+ * Return: 0 success
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
