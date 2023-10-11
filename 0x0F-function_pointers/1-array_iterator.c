8112d0efc203d392c3ef0eeec0358f8873d250d0
Yassine-SE <ytarkaoui@gmail.com>
2023-10-11 20:48:35 +0100

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: param
+ * @size: param
+ * @action: function to pointer param
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
