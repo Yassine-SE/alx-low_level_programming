6c449502c3f6bef751d2c8c8fdb7001b848bbe72
Yassine-SE <ytarkaoui@gmail.com>
2023-10-11 23:45:41 +0100

    Task 3.4

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,16 +12,27 @@
 */
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int num1 , num2;
 	int result = 0;
+	char *operator;
 
-	if (argc == 3)
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[3]);
-		result = (*get_op_func(argv[2]))(a, b);
-	}
+	if (argc != 3)
+		printf("Error\n");
+		exit(98);
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (!get_op_func(operator) || operator[1] != '\0')
+		printf("Error\n");
+		exit(99);
+
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
+		printf("Error\n");
+		exit(100);
+
+	result = get_op_func(operator)(num1, num2);
 	printf("%d\n", result);
 	return (0);
 }
