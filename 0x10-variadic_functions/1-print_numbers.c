adc4060982d2172b97803662b5f032afcc634bd1
Yassine-SE <ytarkaoui@gmail.com>
2023-10-12 20:34:40 +0100

    Task 1.2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,14 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(parm, n);
 	for (i = 0; i < n; i++)
 	{
-		if (!separator)
-			printf("%d", va_arg(parm, int));
-		else
-		{
-			printf("%d", va_arg(parm, int));
-			if (i != (n -1))
-				printf("%s", separator);
-		}
+		printf("%d", va_arg(parm, int));
+		if (separator && i != (n - 1))
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(parm);
