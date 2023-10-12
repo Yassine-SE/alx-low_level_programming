b3cf6665c478f20f47d2d38c32df1487825eaa3c
Yassine-SE <ytarkaoui@gmail.com>
2023-10-12 21:29:29 +0100

    Task 3.5

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,39 +13,35 @@ void print_all(const char * const format, ...)
 {
 	int i;
 	char *string;
-	char *separator;
+	char *separator = "";
 	va_list param;
 
 	va_start(param, format);
-	
-	if (format)
+	while (format[i])
 	{
-		while (format[i])
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%s%c", separator, va_arg(param, int));
-					break;
-				case 'i':
-					printf("%s%d", separator, va_arg(param, int));
-					break;
-				case 'f':
-					printf("%s%f", separator, va_arg(param, double));
-					break;
-				case 's':
-					string = va_arg(param, char *);
-					if (!string)
-						string = "(nil)";
-					printf("%s%s", separator, string);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			separator = ", ";
-			i++;
+			case 'c':
+				printf("%s%c", separator, va_arg(param, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(param, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(param, double));
+				break;
+			case 's':
+				string = va_arg(param, char *);
+				if (!string)
+					string = "(nil)";
+				printf("%s%s", separator, string);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		separator = ", ";
+		i++;
 	}
 	printf("\n");
 	va_end(param);
