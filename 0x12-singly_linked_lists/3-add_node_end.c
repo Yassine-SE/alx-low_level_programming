13cf3a8faeb67315927ed4d82a235a1df535f209
Yassine-SE <ytarkaoui@gmail.com>
2023-10-19 23:42:33 +0100

    Task 3.0

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_node - function
+ * @head: param
+ * @str: param
+ *
+ * Return: 0 success
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node;
+	list_t *temp;
+	int count;
+
+	node = malloc(sizeof(list_t));
+	if (!node || !head || !str)
+		return (NULL);
+
+	count = 0;
+	while (str[count])
+		count++;
+
+	temp = *head;
+	node->str = strdup(str);
+	node->len = count;
+	node->next = NULL;
+	(*head)->next = node;
+
+	if (!head)
+	{
+		*head = node;
+		return (node);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = node;
+	return (node);
+}
