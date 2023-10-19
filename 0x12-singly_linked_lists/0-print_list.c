c728f931760712f429cc6da650be7119306a3d2e
Yassine-SE <ytarkaoui@gmail.com>
2023-10-19 21:21:04 +0100

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - function
+ * @h: param
+ *
+ * Return: 0 success
+*/
+size_t print_list(const list_t *h)
+{
+	size_t n_list = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+
+		h = h->next;
+		n_list++;
+	}
+
+	return (n_list);
+}
