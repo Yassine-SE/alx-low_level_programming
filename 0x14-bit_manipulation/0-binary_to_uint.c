0c26be3c2ee1ee3271771176a3a08dd3d807fd19
Yassine-SE <ytarkaoui@gmail.com>
2023-10-26 21:47:19 +0100

    Task 0.1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,11 +15,21 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	i = 0;
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+
+	i = 0;
+	while (b[i] != '\0')
 	{
 		un_int <<= 1;
 		if (b[i] == '1')
 			un_int += 1;
+		i++;
 	}
 	return (un_int);
 }
