b01b4217f8b61e294a6bf52ef7645d6233f26b9a
Yassine-SE <ytarkaoui@gmail.com>
2023-10-27 22:06:13 +0100

    Task 1.1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,5 +8,21 @@
 */
 void print_binary(unsigned long int n)
 {
+	int bit;
+	int x;
 
+	bit = sizeof(unsigned long int) * 8;
+	while (bit)
+	{
+		if (n & 1L << bit--)
+		{
+			x = 1;
+			_putchar('1');
+		}
+		else if (x)
+			_putchar('0');
+	}
+
+	if (!x)
+		_putchar('0');
 }
