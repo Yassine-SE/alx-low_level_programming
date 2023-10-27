4ad553d5ce58726e5e75d6c940fa5511fd64d739
Yassine-SE <ytarkaoui@gmail.com>
2023-10-27 21:41:36 +0100

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * flip_bits - function
+ * @n: param
+ * @m: param
+ *
+ * Return: 0 success
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i;
+	int bit = 0;
+	unsigned long int x;
+	unsigned long int y = n ^ m;
+
+	i = 63;
+	while (i >= 0)
+	{
+		x = y >> i;
+		if (x & 1)
+			bit++;
+		
+		i++;
+	}
+
+	return (bit);
+}
