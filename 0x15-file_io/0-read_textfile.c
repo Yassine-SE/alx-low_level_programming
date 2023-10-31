e9f9f2a9bac0c1720f67c3595f3a129894508758
Yassine-SE <ytarkaoui@gmail.com>
2023-10-31 20:47:05 +0100

    Init peoject

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - function
+ * @filename: param
+ * @letters: param
+ *
+ * Return: 0 success
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+
+	file = fopen(filename, O_RDONLY);
+
+	bytes = read(file, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	fclose(file);
+	return (bytes);
+}
