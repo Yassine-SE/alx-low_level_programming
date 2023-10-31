33332bd5db97bef701995418bedcc40f8038284b
Yassine-SE <ytarkaoui@gmail.com>
2023-10-31 21:55:41 +0100

    Task 1.10

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes = 0;
 	int len = 0;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
