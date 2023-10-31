e5e7e9665b360562859fd0516b96654d0d95dbaa
Yassine-SE <ytarkaoui@gmail.com>
2023-10-31 21:10:14 +0100

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - function
+ * @filename: param
+ * @text_content: param
+ *
+ * Return: 0 success
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t bytes;
+	int len = 0;
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (0);
+
+	len = strlen(text_content);
+	if (len)
+		bytes = write(file, text_content, len);
+
+	close(file);
+	return (1);
+}
