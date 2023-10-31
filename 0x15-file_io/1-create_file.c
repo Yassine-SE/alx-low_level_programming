0c2ea027faf7346a3f369ea3b0a12432eba31879
Yassine-SE <ytarkaoui@gmail.com>
2023-10-31 21:52:13 +0100

    Task 1.8

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,8 +28,8 @@ int create_file(const char *filename, char *text_content)
 		bytes = write(file, text_content, len);
 
 	close(file);
-	if (bytes != len)
-		return (-1);
-	else
+	if (bytes == len)
 		return (1);
+	else
+		return (-1);
 }
