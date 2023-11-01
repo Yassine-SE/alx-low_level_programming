cf40eac1f466ee67c264a85acf2b68e840a129bc
Yassine-SE <ytarkaoui@gmail.com>
2023-11-01 20:37:37 +0100

    Task 1.13

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,19 +10,26 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-	ssize_t bytes = 0;
+	int bytes = 0;
 	int len = 0;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
 	while (text_content[len] != '\0')
 		len++;
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	bytes = write(file, text_content, len);
+	if (!file)
+		return (-1);
 
-	if (!file || !bytes)
+	if (len)
+		bytes = write(file, text_content, len);
+
+	close(file);
+	if (bytes == len)
+		return (1);
+	else
 		return (-1);
 
 	return (1);
